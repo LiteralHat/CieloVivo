b9f14cb2c97c9824a2ed55e146d44c04ffd5ddf5
LiteralHat <90408956+LiteralHat@users.noreply.github.com>
2024-11-24 09:15:39 +1100

    move charlieboard to cielovivo

---
+++ resources/views/links.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>LiteralLinks</title>
+</head>
+
+<body>
+    <h1>Links</h1>
+    <ul>
+        <li><a href="https://www.tumblr.com/literalhat" target="_blank">Tumblr</a></li>
+        <li><a href="https://x.com/LiteralHat" target="_blank">Twitter</a></li>
+        <li><a href="https://www.instagram.com/literalhat/" target="_blank">Instagram</a></li>
+        <li><a href="https://literalhat.newgrounds.com/" target="_blank">Newgrounds</a></li>
+        <li><a href="https://www.reddit.com/r/literalhat" target="_blank">Reddit</a></li>
+        <li><a href="https://literalhat.bandcamp.com/" target="_blank">Bandcamp</a></li>
+        <li><a href="https://soundcloud.com/literalhat" target="_blank">SoundCloud</a></li>
+        <li><a href="https://www.youtube.com/c/literalhat" target="_blank">YouTube</a></li>
+    </ul>
+
+    <a href="index.html">Back Home</a>
+
+</body>
+
+</html>
